4b3c9f1df28a65cf95ab57a133677b75ad00f25e
Vidush Vishwanath <vidushv@gmail.com>
2016-01-06 22:11:20 -0800

    LOL BEANS

---
+++ timetest.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include "LinkedList.h"
+
+using namespace std;
+
+void RunList(char *);
+
+
+int main ()
+{
+	int userChoice;
+	cout << "      ADT Menu" << endl;
+	cout << "0. Quit" << endl;
+	cout << "1. LinkedList" << endl;
+	cout << "2. CursorList" << endl;
+	cout << "3. StackAr" << endl;
+	cout << "4. StackLi" << endl;
+	cout << "5. QueueAr" << endl;
+	cout << "6. SkipList" << endl;
+	cout << "Your choice >> ";
+	cin >> userChoice;
+
+	return 0;
+
+}
+
+void RunList (char *filename)
+{
+	List<int> list;
+	ListItr<int> listItr = list.zeroth();
+	ifstream inf(filename);
+	char comm, s[512];
+	int value;
+	inf.getline(s, 512);
+	while (inf >> comm >> value)
+	{
+		if (comm == 'i')
+			list.insert(value, listItr);
+		else
+			list.remove(value);
+	}		
+}	
